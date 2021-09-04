ab64c0c27e65a506d43c3366be6aba40cf4bea69
kazurayam <kazuaki.urayama@gmail.com>
2021-09-04 11:28:53 +0900

    fixed a mistake in Test Cases/solution/05

--- Scripts/solution/05_executeTestClosuresMultiThreadsLayoutManaged/Script1614249845231.groovy
+++ Scripts/solution/05_executeTestClosuresMultiThreadsLayoutManaged/Script1614249845231.groovy
@@ -4,13 +4,16 @@ import com.kazurayam.ks.testclosure.TestClosure
 import com.kazurayam.ks.testclosure.TestClosureCollectionExecutor
 import com.kazurayam.ks.windowlayout.BrowserWindowLayoutKeyword as BrowserWindow
 import com.kazurayam.ks.windowlayout.StackingWindowLayoutMetrics
+import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 // load the collection of TestClosures
 List<TestClosure> tclosures = new ArrayList<TestClosure>()
 
 Closure closure = { WebDriver driver, String url ->
+	DriverFactory.changeWebDriver(driver)
 	WebUI.navigateToUrl(url)
+	WebUI.waitForPageLoad(5)
 }
 
 tclosures.add(new TestClosure(closure, ["http://demoaut.katalon.com/"]))
